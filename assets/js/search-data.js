4ca2cf6988bd25213be72db00afd5af527fbdbe4
samuelefonio <86052644+samuelefonio@users.noreply.github.com>
2024-12-28 20:36:09 +0000

    Deploying to gh-pages from @ samuelefonio/samuelefonio.github.io@fc4fedf7b4cc926929291e58c58b2fcf0f0129f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -43,13 +43,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
